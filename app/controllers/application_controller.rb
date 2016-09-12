1a7459942a90e8c47c54d7bc11a04bf1ead8c3a7
rmkothar <rmkothar@syr.edu>
2016-09-12 14:44:02 -0400

    final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :all_categories
     helper_method :all_posts
+
     before_filter :site_search
 
+  
   def all_categories
   	@categories = Category.all
   end
